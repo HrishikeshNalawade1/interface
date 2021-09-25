8f3067ad7295e437911542cf598cac32b81c68b1
Hrishikesh Nalawade <80455130+HrishikeshNalawade1@users.noreply.github.com>
2021-09-25 23:10:54 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace staticc1
+{
+    class Program
+    {
+        int x = 100;
+        static int y=200;
+        public Program(int a)
+        {
+            this.x = a;
+           
+        }
+        static void Main(string[] args)
+        {
+            
+            Program c = new Program(20);
+            Program cc = new Program(320);
+            Console.WriteLine(c.x);
+            Console.WriteLine(cc.x);
+            Console.WriteLine(y);          
+        }
+    }
+}
